d10ff6216f76ef1dc7e36ff8e23090b89a01cc26
Igor Savin <beixiaofeng@gmail.com>
2019-05-09 15:14:11 +0300

    fetched data from my github

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,38 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import fetch from 'isomorphic-unfetch'
+import { Component} from 'react'
 
 
-export default () => (
-  <Layout title="About">
-    <Link href="/">
-      <a>Go home</a>
-    </Link>
-    <p>A React.js developer</p>
-    <img src="/static/React-logo.png" alt="React" height="150px" />
-  </Layout>
-)
+export default class About extends Component {
+  static async getInitialProps(){
+    const res = await fetch('https://api.github.com/users/IgorSavin');
+    const data = await res.json();
+
+    return { user: data };
+  }
+
+  // componentDidMount(){
+  //   fetch('https://api.github.com/users/IgorSavin')
+  //   .then(res => res.json())
+  //   .then(data =>{
+  //     this.setState({
+  //       user:data
+  //     })
+  //   })
+  // }
+
+  render(){
+    const { user } =this.props;
+
+    return (
+      <Layout title="About">
+        <Link href="/">
+          <a>Go home</a>
+        </Link>
+        <p>{user.login}</p>
+        <img src={user.avatar_url} alt="Me" height="150px" />
+      </Layout>
+    )
+  }
+}
