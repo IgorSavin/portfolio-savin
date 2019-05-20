9ff088afef2360c8df3171250c8ab9557a9910d2
Igor Savin <beixiaofeng@gmail.com>
2019-05-20 21:06:23 +0300

    final

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,10 @@
 import Layout from '../components/Layout'
+import { withRouter } from 'next/router'
 
-const Post = ({ url }) =>(
-  <Layout title={url.query.title}>
+const Post = ({ router }) =>(
+  <Layout title={router.query.title}>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ipsam dolores facere, vero assumenda tempore quibusdam quae, tempora consectetur in reprehenderit atque aliquid delectus blanditiis sapiente dolorem cumque veniam et!</p>
   </Layout>
 )
 
-export default Post;
+export default withRouter(Post);
